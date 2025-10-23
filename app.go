9a41dbf74cb4a429a1ab34b1e9e5b2b68b29c76e
优孟 <397317382@qq.com>
2025-10-23 12:37:50 +0900

    feature: debug

--- app.go
+++ app.go
@@ -62,6 +62,14 @@ func main() {
 	}
 
 	log.Println("current OS: ", runtime.GOOS)
+	mqttClient = newMQTTClient()
+	defer func() {
+		if mqttClient != nil {
+			mqttClient.Disconnect(250)
+			mqttClient = nil
+		}
+	}()
+
 	task(username, password)
 }
 
@@ -94,14 +102,6 @@ func getThisValidMonthAndYear() string {
  * Main task
  */
 func task(username, password string) string {
-	mqttClient = newMQTTClient()
-	defer func() {
-		if mqttClient != nil {
-			mqttClient.Disconnect(250)
-			mqttClient = nil
-		}
-	}()
-
 	// 启动浏览器
 	// var la *launcher.Launcher
 
